332ff1faaba4c5203928c9d187b4f80cf49addb0
Oscar Londoño <olondonol@hotmail.com>
2021-05-12 09:30:19 -0500

    Version Inicial

---
+++ src/webparts/cargueArchivos/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICargueArchivosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CargueArchivosWebPartStrings' {
+  const strings: ICargueArchivosWebPartStrings;
+  export = strings;
+}
